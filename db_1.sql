504e26400318451aa62cdf8c3140d8d9ca28af0d
S4NT146O <santizapata359@hotmail.es>
2024-02-19 23:08:18 -0300

    change, ready

--- db_1.sql
+++ db_1.sql
@@ -18,9 +18,7 @@ CREATE TABLE `horno`.`producto` (
   PRIMARY KEY (`ID_Producto`)
 );
 
-ALTER TABLE `horno`.`producto`
-ADD COLUMN `Id_deposito` INT(5) NOT NULL,
-ADD FOREIGN KEY (`Id_deposito`) REFERENCES `deposito`(`Id_deposito`);
+
 
 CREATE TABLE `horno`.`cliente` (
   `ID_Cliente` INT(5) NOT NULL AUTO_INCREMENT,
@@ -43,22 +41,22 @@ CREATE TABLE `horno`.`pedido` (
   FOREIGN KEY (`ID_Cliente`) REFERENCES `cliente`(`ID_Cliente`)
 );
 
-INSERT INTO `horno`.`cliente`('Nombre','Direccion','Telefono')
+INSERT INTO horno.cliente(Nombre,Direccion,Telefono)
 VALUES ('Carlos','Lugones 777','260444444'),
         ('Esteban','Olavarria 152','2604474587'),
         ('Gianfranco','Garnacho 1227','2604877520');
 
-INSERT INTO 'horno'.'producto' ('Nombre','Descript','Precio'),
+INSERT INTO horno.producto (Nombre,Descrip,Precio)
 VALUES ('Ladrillo','Ladrillo comun de toda la vida','50'),
         ('Adobon','Ladrillo de adobon','100');
 
-INSERT INTO 'horno'.'deposito' ('Id_producto','cantidad','fecha')
+INSERT INTO horno.deposito (Id_producto,cantidad,fecha)
 VALUES ('1','1500',current_timestamp()),
         ('2','3500',current_timestamp());
 
-INSERT INTO 'horno', 'pedido' ('ID_deposito','ID_Cliente','Cantidad','PrecioVenta','DireccionEnvio','Fecha')
-VALUES ('1', '1', '2', 'Roca 123', '5,2', current_timestamp());
-        ('1', '1', '2', 'Espanha 123', '2,1', current_timestamp());
+INSERT INTO horno. pedido (ID_deposito,ID_Cliente,Cantidad,PrecioVenta,DireccionEnvio,Fecha)
+VALUES ('1', '1', '2', 'Roca 123', '5,2', current_timestamp()),
+        ('1', '1', '2', 'Espanha 123', '2,1', current_timestamp()),
         ('1', '1', '2', 'Villegas 123', '5,3', current_timestamp());
 
 
